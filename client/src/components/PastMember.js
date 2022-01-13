648b6c6fb6fbee135d30ab36760fafcb3fdba183
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2022-01-12 20:39:22 -0600

    members

---
+++ client/src/components/PastMember.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@mui/material';
+
+const PastMember = ({english, japanese, members}) => {
+	return (
+		<div>
+			<Typography variant="past">{english} GENERATION {japanese}</Typography>
+			{
+				members ? members.map((member, index) => 
+					<Typography key={index}>{member.name} • {member.year}</Typography>
+				) : null
+			}
+		</div>
+	);
+};
+
+PastMember.propTypes = {
+	english: PropTypes.string.isRequired, 
+	japanese: PropTypes.string.isRequired, 
+	members: PropTypes.object.isRequired
+};
+
+export default PastMember;
+
+
+
