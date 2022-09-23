8ef34ed76630e996a029b40a90ddef720b7d2246
ejiang24 <ethan_jiang@brown.edu>
2022-09-23 17:20:14 -0400

    media not centered

--- client/src/pages/Media.js
+++ client/src/pages/Media.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, ImageList, ImageListItem, Box, Grid } from '@mui/material';
 
 const Media = () => {
+	var listOfImages = [];
+
+	function importAll(r) {
+		return r.keys().map(r);
+	}
+
+	listOfImages = importAll(require.context('/public/images/', false, /\.(png|jpe?g|svg)$/));
+
 	return (
-		<Typography>Media</Typography>
+		<Box sx={{ padding : '100px', display: 'flex'}}>
+			<Typography variant='h1'>MEDIA</Typography>
+			<Grid container px={{xs: 4, lg: 12, xl: 16}} pt={{xs: 4, lg: 8}} pb={{xs: 2, lg: 4}}>	
+				<ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+					{listOfImages.map((image) => (
+						<ImageListItem key={image.img}>
+							<img
+								src={image}
+							/>
+						</ImageListItem>
+					))}
+				</ImageList>
+			</Grid>	
+		</Box>
 	);
 };
 
-export default Media;
+export default Media; 
